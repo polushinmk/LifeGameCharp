f3683328bae6919a1ce368a9b09f378487849419
polushinmk <polushin@kell.ru>
2013-09-03 12:49:15 +0400

    upload files

---
+++ LifeGameCharp/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace LifeGameCharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(667, 615);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(691, 130);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(691, 95);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Продолжить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(691, 166);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 28);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(703, 46);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 22);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(712, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Скорость";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(691, 202);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 28);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Сохранить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(691, 238);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 28);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Загрузить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "lsf";
+            this.saveFileDialog1.Filter = "Файл сохранения|*.lsf";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "lsf";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Файл сохранения|*.lsf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(700, 279);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Поколение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(881, 645);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Жизнь";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LifeGameCharp/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LifeGameCharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //разрисовка панели-поля
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+            for(int i=0;i<=500;i+=10)
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Gray)), i, 0, i, 500);
+            }
+            for (int i = 0; i <= 500; i += 10)
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Gray)), 0, i, 500, i);
+            }
+            panredraw();
+        }
+        //2 массива для прорисовки и для логики игры
+        byte[,] coords = new byte[500, 500];
+        byte[,] life = new byte[500, 500];
+        int pokolenie = 0;
+        //файл для сохранения настроек
+        FileInfo f = new FileInfo("temp.dat");
+        //рисовать или удалить клетку
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x=0;
+            int y=0;
+            x=(e.X/10)*10;
+            y=(e.Y/10)*10;
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+            if (coords[x, y] == 1)
+            {
+                g.FillRectangle(new SolidBrush(Color.White), x + 1, y + 1, 8, 8);
+                coords[x, y] = 0;
+                life[x, y] = 0;
+            }
+            else
+            {
+                g.FillRectangle(new SolidBrush(Color.Black), x + 1, y + 1, 8, 8);
+                coords[x, y] = 1;
+                life[x, y] = 1;
+            }
+        }
+        //перерисовка панели
+        private void panredraw()
+        {
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+            for (int j = 0; j < 500; j+=10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    if (coords[i, j] == 1)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.Black), i+1, j+1, 8, 8);
+                    }
+                    else
+                    {
+                        g.FillRectangle(new SolidBrush(Color.White), i+1, j+1, 8, 8);
+                    }
+                }
+            }
+        }
+        //логика игры, включается таймер и всё повторяется снова и снова до его выключения…
+        public void life_engine()
+        {
+                timer1.Start();
+                timer1.Tick += new EventHandler(timer1_Tick);
+        }
+        //вот сама функция происходящая после реакции таймера, реализация логики по правилам
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            byte n = 0;
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+            for (int j = 0; j < 500; j += 10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    n = 0;
+                    //для обычных клеток
+                    if (i > 0 && j > 0 && i < 490 && j < 490)
+                    {
+                        if (life[i - 10, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    //для клеток имеющих меньше 8 соседей
+                    else if (i == 0 && j == 0)
+                    {
+                        if (life[i+10,j]==1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if (i == 490 && j == 490)
+                    {
+                        if (life[i - 10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j-10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j-10] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if (i == 0 && j == 490)
+                    {
+                        if (life[i, j-10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i+10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j-10] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if (i == 490 && j == 0)
+                    {
+                        if (life[i - 10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j+10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j+10] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if ((i == 0 && (j > 10 && j < 490)))
+                    {
+                        if (life[i, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if ((i==490 && (j > 10 && j < 490)))
+                    {
+                        if (life[i - 10, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if ((j == 0 && (i > 10 && i < 490)))
+                    {
+                        if (life[i - 10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j + 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    else if ((j == 490 && (i > 10 && i < 490)))
+                    {
+                        if (life[i - 10, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j - 10] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i - 10, j] == 1)
+                        {
+                            n++;
+                        }
+                        if (life[i + 10, j] == 1)
+                        {
+                            n++;
+                        }
+                    }
+                    //проверка жива или нет клетка…вот весь этот цикл и жрёт кучу процессорного времени, когда размер поля небольшой, как тут 200*200 то ничего не чувствуется, но вот потом…
+                        if (life[i, j] == 0 && n == 3)
+                        {
+                            coords[i, j] = 1;
+                        }
+                        if ((life[i, j] == 1 && n < 2) || (life[i,j] == 1 && n > 3))
+                        {
+                            coords[i,j]=0;
+                        }
+                }
+            }
+            //перерисовать панель с учётом оживших и умерших клеток
+            panredraw();
+            update_life();
+            pokolenie++;
+            label2.Text = "Поколение " + Convert.ToString(pokolenie);
+        }
+        //маленькая функция для синхронизации 2 массивов…может быть я туплю, ноиначе никак не сделать, иначе логика работы нарушиться
+        public void update_life()
+        {
+            for (int j = 0; j < 500; j += 10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    life[i, j] = coords[i, j];
+                }
+            }
+        }
+        //для сохранения настроек по нажатию кнопки «начать» (так как нужно сохранение именно начальной конфигурации а не конечной)
+        public void save_sett()
+        {
+            StreamWriter sw = f.CreateText();
+            for (int j = 0; j < 500; j += 10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    sw.Write(coords[i, j]);
+                }
+            }
+            sw.Close();
+        }
+        //для загрузки настроек
+        public void load_sett()
+        {
+            openFileDialog1.ShowDialog();
+            openFileDialog1.FileOk += new CancelEventHandler(openFileDialog1_FileOk);
+            string n = openFileDialog1.FileName;
+            StreamReader sr = File.OpenText(n);
+            string input = sr.ReadToEnd();
+            int tmp=0;
+            for (int j = 0; j < 500; j += 10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    coords[i, j] = Convert.ToByte(input.Substring(tmp, 1));
+                    tmp++;
+                }
+            }
+            sr.Close();
+            update_life();
+            panredraw();
+        }
+
+        void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string n = openFileDialog1.FileName;
+            StreamReader sr = File.OpenText(n);
+            string input = sr.ReadToEnd();
+            int tmp = 0;
+            for (int j = 0; j < 500; j += 10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    coords[i, j] = Convert.ToByte(input.Substring(tmp, 1));
+                    tmp++;
+                }
+            }
+            sr.Close();
+            update_life();
+            panredraw();
+        }
+        //полное сохранение настроек по нажатию кнопки «сохранить»
+        public void save_sett_full()
+        {
+            saveFileDialog1.ShowDialog();
+            string n = saveFileDialog1.FileName;
+            try
+            {
+                f.MoveTo(n);
+            }
+            catch (Exception ex)
+            {
+                f.Replace(n, n + ".bak");
+            }
+        }
+        //запуск
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(numericUpDown1.Value);
+            life_engine();
+            button3.Visible = true;
+            button1.Visible = false;
+            button4.Enabled = false;
+            numericUpDown1.Enabled = false;
+            save_sett();
+            button5.Enabled = false;
+            button6.Enabled = false;
+        }
+        //остановить
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button4.Enabled = true;
+            numericUpDown1.Enabled = true;
+            button5.Enabled = true;
+        }
+        //продолжить
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(numericUpDown1.Value);
+            life_engine();
+            button4.Enabled = false;
+            numericUpDown1.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+        }
+        //очистить
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            button1.Visible = true;
+            for (int j = 0; j < 500; j += 10)
+            {
+                for (int i = 0; i < 500; i += 10)
+                {
+                    life[i, j] = 0;
+                    coords[i, j] = 0;
+                }
+            }
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, 500, 500);
+            for (int i = 0; i <= 500; i += 10)
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Gray)), i, 0, i, 500);
+            }
+            for (int i = 0; i <= 500; i += 10)
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Gray)), 0, i, 500, i);
+            }
+            button5.Enabled = false;
+            button6.Enabled = true;
+            pokolenie = 0;
+            label2.Text = "Поколение " + Convert.ToString(pokolenie);
+        }
+        //загрузка
+        private void button6_Click(object sender, EventArgs e)
+        {
+            load_sett();
+        }
+        //сохранить
+        private void button5_Click(object sender, EventArgs e)
+        {
+            save_sett_full();
+        }
+
+        public void habrahabr()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
